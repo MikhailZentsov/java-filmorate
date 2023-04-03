5be11508ea134a211fd6da8fcf7c612810ce40d9
zentsov.mikhail <zentsov.mikhail@yandex.ru>
2023-04-03 19:27:35 +0300

    style: добавлена пустая строка

--- src/main/java/ru/yandex/practicum/filmorate/validator/FilmReleaseDateConstraintValidator.java
+++ src/main/java/ru/yandex/practicum/filmorate/validator/FilmReleaseDateConstraintValidator.java
@@ -7,6 +7,7 @@ import java.time.LocalDate;
 
 public class FilmReleaseDateConstraintValidator implements ConstraintValidator<FilmReleaseDateConstraint, LocalDate> {
     private static final LocalDate MIN_RELEASE_DATE = LocalDate.of(1895, 12,28);
+
     @Override
     public boolean isValid(LocalDate releaseDate, ConstraintValidatorContext constraintValidatorContext) {
         return releaseDate == null || !releaseDate.isBefore(MIN_RELEASE_DATE);
--- src/main/java/ru/yandex/practicum/filmorate/validator/UserLoginConstraintValidator.java
+++ src/main/java/ru/yandex/practicum/filmorate/validator/UserLoginConstraintValidator.java
@@ -4,6 +4,7 @@ import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
 public class UserLoginConstraintValidator implements ConstraintValidator<UserLoginConstraint, String> {
+
     @Override
     public boolean isValid(String login, ConstraintValidatorContext constraintValidatorContext) {
         return login == null || !login.contains(" ");
