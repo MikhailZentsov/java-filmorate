0ea4efb1f83b5c0e3659455ca6aa5f83146fb9ae
zentsov.mikhail <zentsov.mikhail@yandex.ru>
2023-05-24 04:09:30 +0300

    style: поправлен стиль

--- src/main/java/ru/yandex/practicum/filmorate/model/Mpa.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Mpa.java
@@ -30,13 +30,11 @@ public enum Mpa {
 
     @JsonCreator
     public static Mpa forValues(@JsonProperty("id") int id) {
-
         for (Mpa mpa : Mpa.values()) {
             if (mpa.id == id) {
                 return mpa;
             }
         }
-
         return null;
     }
 }
